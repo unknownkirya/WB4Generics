78cf26553a9a096bbe7aebaebfa1574b1aa70ca7
unknownkirya <unknownkirya@gmail.com>
2024-07-28 14:11:05 +0300

    Initial Commit

---
+++ WB4Generics/WB4GenericsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WB4GenericsApp.swift
+//  WB4Generics
+//
+//  Created by Kirill on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WB4GenericsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
